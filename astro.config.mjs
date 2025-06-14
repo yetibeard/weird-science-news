f30f6ed099ab7ebe22d7e3e205013a952bf1e324
Yeti Beard <yetibeard420@gmail.com>
2025-06-14 06:29:22 -0500

    add SEO component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [tailwind()],
+  site: "https://weird-science-news.com",
+  integrations: [
+    tailwind(),
+    sitemap({
+      changefreq: "weekly",
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
 });
