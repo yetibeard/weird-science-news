89669624b68be64d2f153fd398bd9b33ff8c08f5
Yeti Beard <yetibeard420@gmail.com>
2025-06-14 07:00:40 -0500

    stupid sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,5 @@ import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://weird-science-news.com",
-  integrations: [
-    tailwind(),
-    sitemap({
-      customPages: [`${new URL("https://weird-science-news.com").origin}`],
-      filter: (page) => !page.includes("404"),
-    }),
-  ],
+  integrations: [tailwind(), sitemap()],
 });
